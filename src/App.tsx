446e9edb7fd9b055a3417c2fcf0801dbe4cbab90
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-08-10 19:52:25 +0300

    #3 base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/App.css';
+import { SingleData } from './components/SingleData';
+import { Hang } from './components/Hang/Hang';
+import { KeyBoard } from './components/Letters/KeyBoard';
+import { Word } from './components/Word/Word';
+import Notebook from './assets/notebook.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='game'>
+      <div className="container column">
+        <div className="row1">
+          <div className="left flex column">
+            <SingleData>Round: 1</SingleData>
+            <SingleData>Record: 2</SingleData>
+          </div>
+          <div className="right">
+            <SingleData>Subject: technology</SingleData>
+          </div>
+        </div>
+        <Hang/>
+        <img className='notebook' src={Notebook} alt="" />
+        <div className="words">
+          <Word word={'abc'}/>
+          <KeyBoard/>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App;
