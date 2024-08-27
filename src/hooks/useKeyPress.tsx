091fab80f45bc3b3d7169f743ff989e423252554
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-08-27 18:21:27 +0300

    #27 fixed key press

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
+import { keyPressAllowed } from "../utils/helpers"
 
 export const useKeyPress = (handleGuess: (letter: string) => void) => {
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if(e.key.length === 1) handleGuess(e.key.toLowerCase())
+      if(keyPressAllowed(e.key)) handleGuess(e.key.toLowerCase())
     }
     window.addEventListener('keydown', handleKeyDown)
     
